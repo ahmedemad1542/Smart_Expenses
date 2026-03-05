6f1395afb1c25d5d9cfa60c7b4c2e290aec5e22e
AHMED-EMAD\Mohamed Badr <ahmedemad153717@gmail.com>
2026-03-05 17:57:05 +0200

    Add colors

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+
+  static const Color primaryGreen = Color(0xFF00D09E);
+  static const Color lightGreen = Color(0xFFDCF3DF);
+  
+  static const Color primaryBlue = Color(0xFF0066FB);
+  static const Color lightBlue = Color(0xFF3197FC);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
